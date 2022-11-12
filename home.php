4706929b110c5f88cce7f5dad2137a3b6875d3ef
Sammy Joseph <sam.tab.paz@gmail.com>
2022-11-12 10:20:32 -0500

    home.php blog

--- home.php
+++ home.php
@@ -6,11 +6,21 @@
 get_header(); ?>
 
 <div class="container">
-    <div class="row">
-        <div class="col-12">
-            <p>Este es el home.php</p>
-        </div>
-    </div>
+    <?php if(have_posts()): ?>
+        <?php while(have_posts()): the_post(); ?>
+            <div class="row mb-4">
+                <div class="col-lg-4 col-xl-4">
+                    <!-- 2 opciones para que la imagen tenga la clase fluida (img-fluid) -->
+                    <!-- <?php the_post_thumbnail('full', array('class' => 'img-fluid attachment-post-thumbnail size-post-thumbnail')); ?> -->
+                    <img src="<?php the_post_thumbnail_url($post->ID) ?>" class="img-fluid" alt="">
+                </div>
+                <div class="col-lg-8 col-xl-8">
+                    <?php the_title(); ?>
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        <?php endwhile; ?>
+    <?php endif; ?>
 </div>
 
-<?php get_footer();
+<?php get_footer(); ?>
