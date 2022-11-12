2ad18a5f7754b8ab36e9d2081c52079b0d2e6d3b
SammyJoseph <sam.tab.paz@gmail.com>
2022-11-12 13:58:23 -0500

    blog page

--- home.php
+++ home.php
@@ -5,17 +5,21 @@
 
 get_header(); ?>
 
-<div class="container">
+<div class="container mt-4">
     <?php if(have_posts()): ?>
         <?php while(have_posts()): the_post(); ?>
             <div class="row mb-4">
                 <div class="col-lg-4 col-xl-4">
                     <!-- 2 opciones para que la imagen tenga la clase fluida (img-fluid) -->
                     <!-- <?php the_post_thumbnail('full', array('class' => 'img-fluid attachment-post-thumbnail size-post-thumbnail')); ?> -->
-                    <img src="<?php the_post_thumbnail_url($post->ID) ?>" class="img-fluid" alt="">
+                    <a href="<?php the_permalink($post->ID) ?>">
+                        <img src="<?php the_post_thumbnail_url($post->ID) ?>" class="img-fluid" alt="">
+                    </a>
                 </div>
                 <div class="col-lg-8 col-xl-8">
-                    <?php the_title(); ?>
+                    <a href="<?php the_permalink($post->ID) ?>">
+                        <?php the_title(); ?>
+                    </a>
                     <?php the_content(); ?>
                 </div>
             </div>
---
+++ single.php
@@ -0,0 +1,23 @@
+<?php 
+/* 
+*   Esta es la página para cada entrada del blog
+*/
+get_header(); ?>
+
+<div class="container">
+    <div class="row mt-4">
+        <div class="col-sm-12 col-md-8 col-lg-7">
+            <div class="imagen">
+                <img src="<?php echo get_the_post_thumbnail_url($post->ID) ?>" class="img-fluid" alt="">
+            </div>
+            <div class="titulo">
+                <h3><?php echo get_the_title() ?></h3>
+            </div>
+            <div class="contenido">
+                <?php the_content(); ?>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer(); ?>
